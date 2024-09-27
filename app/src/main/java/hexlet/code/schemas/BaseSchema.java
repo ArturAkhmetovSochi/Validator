31a397b649939c6a7c55d48fe203f108ce2835c3
Artur Akhmetov <artur.akh@internet.ru>
2024-09-27 11:32:07 +0300

    change BaseSchema to BaseSchema<T>

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public abstract class BaseSchema {
+public abstract class BaseSchema<T> {
 
     protected Boolean required = false;
     private final List<Predicate<Object>> predicates = new ArrayList<>();
